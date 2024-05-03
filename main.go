2d3f6e374301384ad1dfb838424a3ab7eb66c5fb
bupd <bupdprasanth@gmail.com>
2024-05-04 01:57:14 +0530

    add simple get request

--- main.go
+++ main.go
@@ -3,6 +3,11 @@ package main
 import "github.com/gofiber/fiber/v2"
 
 func main() {
-  app := fiber.New()
-  app.Listen(":5000")
+	app := fiber.New()
+	app.Get("/", handleMain)
+	app.Listen(":4444")
+}
+
+func handleMain(c *fiber.Ctx) error {
+	return c.JSON(map[string]string{"msgs": "hello I am running in the server"})
 }
