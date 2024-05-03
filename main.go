d27f140a9ba60e3ca6881bc8df4ab5656f7b70c6
bupd <bupdprasanth@gmail.com>
2024-05-04 02:59:00 +0530

    update: main with basic user handlers and health

--- main.go
+++ main.go
@@ -1,20 +1,31 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
 import (
 	"flag"
 
+	"github.com/bupd/hotelmanager/api"
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	app := fiber.New()
+
 	listenAddr := flag.String("listenAddr", ":4444", "The listenAddr of the api Server")
 	flag.Parse()
+
+	apiv1 := app.Group("/api/v1")
+	apiv1.Get("/healthz", handleHealthz)
+	apiv1.Get("/user", api.HandleGetUser)
+	apiv1.Get("/users", api.HandleGetUsers)
+
 	app.Get("/", handleMain)
 	app.Listen(*listenAddr)
 }
 
 func handleMain(c *fiber.Ctx) error {
 	return c.JSON(map[string]string{"msgs": "hello I am running in the server"})
+}
+
+func handleHealthz(c *fiber.Ctx) error {
+	return c.JSON(map[string]int{"status": 200})
 }
