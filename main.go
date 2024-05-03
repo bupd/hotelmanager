378702cfeb48e8b2fc97e09e2e05b5e027d4ee2c
bupd <bupdprasanth@gmail.com>
2024-05-04 02:57:29 +0530

    update listenAddr

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
 import "github.com/gofiber/fiber/v2"
+import (
+	"flag"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	app := fiber.New()
+	listenAddr := flag.String("listenAddr", ":4444", "The listenAddr of the api Server")
+	flag.Parse()
 	app.Get("/", handleMain)
-	app.Listen(":4444")
+	app.Listen(*listenAddr)
 }
 
 func handleMain(c *fiber.Ctx) error {
