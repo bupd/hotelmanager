21690ecb16d66cc723f372c4d31a46447c59f0e4
bupd <bupdprasanth@gmail.com>
2024-05-04 11:10:54 +0530

    add: start mongodb.service specific to the arch linux in Makefile

--- Makefile
+++ Makefile
@@ -2,6 +2,7 @@ build:
 	@go build -o bin/api
 
 run: build
+	@sudo systemctl start mongodb.service
 	@./bin/api
 
 test:
--- api/user_handler.go
+++ api/user_handler.go
@@ -1,18 +1,23 @@
 package api
 
 import (
+	"fmt"
+
 	"github.com/bupd/hotelmanager/types"
 	"github.com/gofiber/fiber/v2"
 )
 
 func HandleGetUser(c *fiber.Ctx) error {
-	return c.JSON(map[string]string{"user": "kumar"})
+	id := c.Params("id")
+	fmt.Println(id)
+	return c.JSON(map[string]string{"user": id})
 }
 
 func HandleGetUsers(c *fiber.Ctx) error {
 	u := types.User{
 		FirstName: "James",
 		LastName:  "Kumar",
+		ID:        "potta",
 	}
 
 	return c.JSON(u)
