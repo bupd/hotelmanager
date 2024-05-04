398b7ac219406094c6d865ff943520e6bf7b9302
bupd <bupdprasanth@gmail.com>
2024-05-05 01:23:01 +0530

    add: TESTS for user_handler

--- api/user_handler_test.go
+++ api/user_handler_test.go
@@ -1,17 +1,23 @@
 package api
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/bupd/hotelmanager/db"
+	"github.com/bupd/hotelmanager/types"
+	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 const (
 	testdburi    string = "mongodb://localhost:27017"
-	testdbname   string = "hotelmanager"
+	testdbname   string = "hotelmanager-test"
 	testuserColl string = "users"
 )
 
@@ -31,12 +37,64 @@ func setup(t *testing.T) *testdb {
 		panic(err)
 	}
 	return &testdb{
-		UserStore: db.NewMongoUserStore(client),
+		UserStore: db.NewMongoUserStore(client, testdbname),
 	}
 }
 
 func TestPotta(t *testing.T) {
 	tdb := setup(t)
 	defer tdb.teardown(t)
-	t.Fail()
+
+	app := fiber.New()
+	userHandler := NewUserHandler(tdb.UserStore)
+	app.Post("/", userHandler.HandlePostUser)
+
+	// Define multiple user creation parameters
+	params := types.CreateUserParams{
+		Email: "test@test.com", FirstName: "Test", LastName: "User", Password: "password2",
+	}
+	// Add more users as needed
+
+	// Convert user creation parameters to JSON
+	b, _ := json.Marshal(params)
+
+	// Create HTTP request with JSON payload
+	req := httptest.NewRequest("POST", "/", bytes.NewReader(b))
+	req.Header.Add("Content-Type", "application/json")
+
+	// Send request to the application
+	resp, err := app.Test(req)
+	if err != nil {
+		t.Fatalf("Failed to send request: %v", err)
+	}
+
+	// Check response status
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf("Expected status %d; got %d", http.StatusOK, resp.StatusCode)
+	}
+
+	// Decode response body to get user details
+	var user types.User
+	err = json.NewDecoder(resp.Body).Decode(&user)
+	if err != nil {
+		t.Fatalf("Failed to decode response body: %v", err)
+	}
+
+	if len(user.ID) == 0 {
+		t.Errorf("Expected userID to be set")
+	}
+	if len(user.EncryptedPassword) > 0 {
+		t.Errorf("Fatal Error returning Password!!!")
+	}
+	if user.FirstName != params.FirstName {
+		t.Errorf("Expected %s but got %s", params.FirstName, user.FirstName)
+	}
+	if user.LastName != params.LastName {
+		t.Errorf("Expected %s but got %s", params.LastName, user.LastName)
+	}
+	if user.Email != params.Email {
+		t.Errorf("Expected %s but got %s", params.Email, user.Email)
+	}
+
+	defer resp.Body.Close()
 }
