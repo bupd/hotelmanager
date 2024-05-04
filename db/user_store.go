f7c82704f3adc2fd1a08b9cb722c8fbdb302df28
bupd <bupdprasanth@gmail.com>
2024-05-04 12:21:02 +0530

    update: user_store to get user from mongo

--- db/user_store.go
+++ db/user_store.go
@@ -1,8 +1,43 @@
 package db
 
-import "github.com/bupd/hotelmanager/types"
+import (
+	"context"
+
+	"github.com/bupd/hotelmanager/types"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+const (
+	DBNAME   = "hotelmanager"
+	userColl = "users"
+)
 
 type UserStore interface {
-	GetUserById(string) (*types.User, error)
+	GetUserById(context.Context, string) (*types.User, error)
 }
 
+type MongoUserStore struct {
+	client *mongo.Client
+	coll   *mongo.Collection
+}
+
+func NewMongoUserStore(c *mongo.Client) *MongoUserStore {
+	return &MongoUserStore{
+		client: c,
+		coll:   c.Database(DBNAME).Collection(userColl),
+	}
+}
+
+func (s *MongoUserStore) GetUserById(ctx context.Context, id string) (*types.User, error) {
+	var user types.User
+	oid, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return nil, err
+	}
+	if err := s.coll.FindOne(ctx, bson.M{"_id": oid}).Decode(&user); err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
