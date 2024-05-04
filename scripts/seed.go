b2145e73c739632398e239e79f469d35db3558da
bupd <bupdprasanth@gmail.com>
2024-05-05 02:35:50 +0530

    seed: file init

--- scripts/seed.go
+++ scripts/seed.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/bupd/hotelmanager/db"
+	"github.com/bupd/hotelmanager/types"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
 
 func main() {
+	ctx := context.Background()
+
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(db.DBURI))
+	if err != nil {
+		panic(err)
+	}
+
+	hotelStore := db.NewMongoHotelStore(client, db.DBNAME)
+
+	hotel := types.Hotel{
+		Name:     "Kumarii",
+		Location: "Singhii",
+	}
+	room := types.Room{
+		Type:      types.SingleRoomType,
+		BasePrice: 99.9,
+	}
+
+	_ = room
+
+	insertedHotel, err := hotelStore.InsertHotel(ctx, &hotel)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(insertedHotel)
 	fmt.Println("seeding the database..")
 }
