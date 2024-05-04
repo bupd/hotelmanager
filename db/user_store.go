497d3a708dbadb4841d0b06840da63d808d77b9f
bupd <bupdprasanth@gmail.com>
2024-05-04 17:46:25 +0530

    update: user store to store the New User

--- db/user_store.go
+++ db/user_store.go
@@ -17,6 +17,7 @@ const (
 type UserStore interface {
 	GetUserById(context.Context, string) (*types.User, error)
 	GetUsers(context.Context) ([]*types.User, error)
+	CreateUser(context.Context, *types.User) (*types.User, error)
 }
 
 type MongoUserStore struct {
@@ -31,6 +32,15 @@ func NewMongoUserStore(c *mongo.Client) *MongoUserStore {
 	}
 }
 
+func (s *MongoUserStore) CreateUser(ctx context.Context, user *types.User) (*types.User, error) {
+	res, err := s.coll.InsertOne(ctx, user)
+	if err != nil {
+		return nil, err
+	}
+	user.ID = res.InsertedID.(primitive.ObjectID)
+	return user, nil
+}
+
 func (s *MongoUserStore) GetUsers(ctx context.Context) ([]*types.User, error) {
 	var users []*types.User
 	cur, err := s.coll.Find(ctx, bson.M{})
@@ -40,15 +50,7 @@ func (s *MongoUserStore) GetUsers(ctx context.Context) ([]*types.User, error) {
 	defer cur.Close(ctx) // Close the cursor once done
 
 	// Iterate through the cursor and decode each document into a user
-	for cur.Next(ctx) {
-		var user *types.User
-		if err := cur.Decode(&user); err != nil {
-			return nil, err
-		}
-		users = append(users, user)
-	}
-
-	if err := cur.Err(); err != nil {
+	if err := cur.All(ctx, &users); err != nil {
 		return nil, err
 	}
 
