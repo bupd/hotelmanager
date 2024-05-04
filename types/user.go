5dec66a9ff69a03cd552d67041c3fe5d641b46ca
bupd <bupdprasanth@gmail.com>
2024-05-04 22:48:04 +0530

    add: tobson method for update user

--- types/user.go
+++ types/user.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"regexp"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"golang.org/x/crypto/bcrypt"
 )
@@ -14,6 +15,24 @@ const (
 	minLastNameLen  = 2
 	minPasswordLen  = 8
 )
+
+type UpdateUserParams struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+}
+
+func (p UpdateUserParams) ToBson() bson.M {
+	m := bson.M{}
+
+	if len(p.FirstName) > 0 {
+		m["firstName"] = p.FirstName
+	}
+	if len(p.LastName) > 0 {
+		m["lastName"] = p.LastName
+	}
+
+	return m
+}
 
 type CreateUserParams struct {
 	FirstName string `json:"firstName"`
@@ -56,7 +75,7 @@ func isEmailValid(e string) bool {
 type User struct {
 	ID                primitive.ObjectID `bson:"_id,omitempty"     json:"id,omitempty"`
 	FirstName         string             `bson:"firstName"         json:"firstName"`
-	LastName          string             `bson:"LastName"          json:"LastName"`
+	LastName          string             `bson:"lastName"          json:"lastName"`
 	Email             string             `bson:"email"             json:"email"`
 	EncryptedPassword string             `bson:"encryptedPassword" json:"-"`
 }
