9b0e3415b1266125c74d6f3bd1418eb5efa9aa1c
bupd <bupdprasanth@gmail.com>
2024-05-05 12:41:10 +0530

    add: update hotel when room is added

--- db/hotel_store.go
+++ db/hotel_store.go
@@ -4,12 +4,14 @@ import (
 	"context"
 
 	"github.com/bupd/hotelmanager/types"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type HotelStore interface {
 	InsertHotel(context.Context, *types.Hotel) (*types.Hotel, error)
+	Update(context.Context, bson.M, bson.M) error
 }
 
 type MongoHotelStore struct {
@@ -24,6 +26,11 @@ func NewMongoHotelStore(client *mongo.Client, dbname string) *MongoHotelStore {
 	}
 }
 
+func (s *MongoHotelStore) Update(ctx context.Context, filter bson.M, update bson.M) error {
+	_, err := s.coll.UpdateOne(ctx, filter, update)
+	return err
+}
+
 func (s *MongoHotelStore) InsertHotel(
 	ctx context.Context,
 	hotel *types.Hotel,
@@ -32,7 +39,8 @@ func (s *MongoHotelStore) InsertHotel(
 	if err != nil {
 		return nil, err
 	}
-  hotel.ID = resp.InsertedID.(primitive.ObjectID)
+
+	hotel.ID = resp.InsertedID.(primitive.ObjectID)
 
 	return hotel, nil
 }
