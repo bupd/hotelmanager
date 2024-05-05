1ef8ca3bcd6275e2d7afc2c8cf07238a50dfd85d
bupd <bupdprasanth@gmail.com>
2024-05-05 12:41:38 +0530

    add: multiple rooms to hotel and drop db on every seed run

--- scripts/seed.go
+++ scripts/seed.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/bupd/hotelmanager/db"
 	"github.com/bupd/hotelmanager/types"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -19,32 +20,45 @@ func main() {
 		panic(err)
 	}
 
+	if err := client.Database(db.DBNAME).Drop(ctx); err != nil {
+		log.Fatal("the db is dropping..")
+	}
+
 	hotelStore := db.NewMongoHotelStore(client, db.DBNAME)
-	roomStore := db.NewMongoRoomStore(client, db.DBNAME)
+	roomStore := db.NewMongoRoomStore(client, db.DBNAME, hotelStore)
 
 	hotel := types.Hotel{
 		Name:     "Kumarii",
 		Location: "Singhii",
+		Rooms:    []primitive.ObjectID{},
 	}
-	room := types.Room{
-		Type:      types.SingleRoomType,
-		BasePrice: 99.9,
+	rooms := []types.Room{
+		{
+			Type:      types.SingleRoomType,
+			BasePrice: 99.9,
+		},
+		{
+			Type:      types.SingleRoomType,
+			BasePrice: 99.9,
+		},
 	}
-
-	_ = room
 
 	insertedHotel, err := hotelStore.InsertHotel(ctx, &hotel)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	room.HotelID = insertedHotel.ID
-	insertedRoom, err := roomStore.InsertRoom(ctx, &room)
-	if err != nil {
-		log.Fatal(err)
+	for _, r := range rooms {
+		r.HotelID = insertedHotel.ID
+		insertedRoom, err := roomStore.InsertRoom(ctx, &r)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(insertedHotel)
+		fmt.Println("--- above is hotel and below is room.")
+		fmt.Println(insertedRoom, "this is room")
 	}
 
-	fmt.Println(insertedHotel)
-	fmt.Println(insertedRoom)
 	fmt.Println("seeding the database..")
 }
